bb610039eadb3b5325ed945d7c33788ef4f04e41
Henry Lee <henry.lee@quad.tw>
2024-03-19 16:19:50 +0800

    feat: 建立四則運算以及位數設置的 api

--- app/main.go
+++ app/main.go
@@ -2,34 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	calculator := NewCalculator()
-	calculator.SetDigits(5)
-	answer, err := calculator.ComputeV1(" 5 * 3 + 1 + 6 / 2")
 
-	if err != nil{
-		fmt.Printf("err: %+v\n", err)
-		return
-	}
+	server := gin.Default()
+	server.GET("/formula", func(ctx *gin.Context) {
+		value := ctx.Query("value")
+		answer, err := calculator.ComputeV2(value)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"value": value,
+				"err":   fmt.Sprintf("err: %+v", err),
+			})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{
+				"answer": answer,
+			})
+		}
+	})
+	server.POST("/digits", func(ctx *gin.Context) {
+		var postData PostDigits
+		if err := ctx.BindJSON(&postData); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-	fmt.Printf("answer: %s\n", answer)
-
-	// server := gin.Default()
-	// server.GET("/formula", func(ctx *gin.Context) {
-	// 	value := ctx.Query("value")
-	// 	answer, err := calculator.ComputeV1(value)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusBadRequest, gin.H{
-	// 			"value": value,
-	// 			"err":   fmt.Sprintf("err: %+v", err),
-	// 		})
-	// 	} else {
-	// 		ctx.JSON(http.StatusOK, gin.H{
-	// 			"answer": answer,
-	// 		})
-	// 	}
-	// })
-	// server.Run()
+		calculator.SetDigits(postData.Digits)
+		ctx.JSON(http.StatusOK, gin.H{"msg": "OK"})
+	})
+	server.Run()
 }
---
+++ app/request.go
@@ -0,0 +1,5 @@
+package main
+
+type PostDigits struct {
+	Digits int `json:"digits"`
+}
