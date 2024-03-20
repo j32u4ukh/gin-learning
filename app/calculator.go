284d5c65a26b460b4cbe46370c9ad3a7e1d2cd6a
Henry Lee <henry.lee@quad.tw>
2024-03-20 13:27:14 +0800

    feat: 考慮負數的情況

--- app/calculator.go
+++ app/calculator.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 /*
@@ -20,8 +21,10 @@ const (
 	TypeNone int = iota
 	// Number 0-9 .
 	TypeNumber
-	// Operator +-*/
+	// Operator +*/
 	TypeOperator
+	// Operator -
+	TypeMinus
 )
 
 type Calculator struct {
@@ -40,194 +43,6 @@ func (c *Calculator) SetDigits(digits int) {
 		digits = 0
 	}
 	c.digits = digits
-}
-
-// a + b - c * d - e / f
-func (c Calculator) ComputeV1(formula string) (string, error) {
-	elements, err := c.ParseFormula(formula)
-	if err != nil {
-		return "", err
-	}
-	// muliply or divide
-	elements, err = c.computeAndSquash(elements, "*", "/")
-	if err != nil {
-		return "", err
-	}
-	// plus or minus
-	elements, err = c.computeAndSquash(elements, "+", "-")
-	if err != nil {
-		return "", err
-	}
-	if len(elements) != 1 {
-		fmt.Printf("elements: %+v\n", elements)
-		return "", errors.New("unexception elements in formula")
-	}
-	return elements[0], nil
-}
-
-func (c Calculator) computeAndSquash(elements []string, targets ...string) ([]string, error) {
-	index := c.FindIndex(elements, targets...)
-	var v1, v2, op, result string
-	var prev, next []string
-	var err error
-	for index != -1 {
-		if index == 0 || index == len(elements)-1 {
-			return nil, errors.New("error format of formula")
-		}
-		v1 = elements[index-1]
-		op = elements[index]
-		v2 = elements[index+1]
-		if c.GetElementType(v1) != TypeNumber || c.GetElementType(v2) != TypeNumber {
-			return nil, errors.New("error format of formula")
-		}
-		result, err = c.compute(v1, op, v2)
-		if err != nil {
-			return nil, errors.New("invalid element of formula")
-		}
-		prev = elements[:index-1]
-		next = elements[index+2:]
-		elements = append(prev, result)
-		elements = append(elements, next...)
-		index = c.FindIndex(elements, targets...)
-	}
-	return elements, nil
-}
-
-func (c Calculator) compute(v1 string, op string, v2 string) (string, error) {
-	var f1, f2 float64
-	var err error
-	if f1, err = strconv.ParseFloat(v1, 64); err != nil {
-		return "", errors.New("cannot convert element to number")
-	}
-	if f2, err = strconv.ParseFloat(v2, 64); err != nil {
-		return "", errors.New("cannot convert element to number")
-	}
-	var result float64
-	switch op {
-	case "+":
-		result = f1 + f2
-	case "-":
-		result = f1 - f2
-	case "*":
-		result = f1 * f2
-	case "/":
-		result = f1 / f2
-	default:
-		return "", errors.New("invalid operator")
-	}
-	format := fmt.Sprintf("%%.%df", c.digits)
-	// fmt.Printf("format: %s, result: %f\n", format, result)
-	return fmt.Sprintf(format, result), nil
-}
-
-func (c Calculator) FindIndex(elements []string, targets ...string) int {
-	var t string
-	for i, e := range elements {
-		for _, t = range targets {
-			if e == t {
-				return i
-			}
-		}
-	}
-	return -1
-}
-
-func (c Calculator) ParseFormula(formula string) ([]string, error) {
-	var currType, t int = TypeNone, TypeNone
-	var crossType int
-	elements := []string{}
-	rs := []rune{}
-	for _, f := range formula {
-		t = c.GetType(f)
-		// fmt.Printf("'%s' -> type: %d\n", string(f), t)
-		crossType = 10*currType + t
-		// fmt.Printf("currType: %d, crossType: %d\n", currType, crossType)
-		switch crossType {
-		// None -> Number
-		case 1:
-			fallthrough
-		// Operator -> Number
-		case 21:
-			rs = append(rs, f)
-		// None -> Operator
-		case 2:
-			elements = append(elements, string(f))
-			// fmt.Printf("Append: '%s'\n", string(f))
-		// Number -> None
-		case 10:
-			elements = append(elements, string(rs))
-			rs = rs[:0]
-		// Number -> Number
-		case 11:
-			rs = append(rs, f)
-		// Number -> Operator
-		case 12:
-			elements = append(elements, string(rs))
-			rs = rs[:0]
-			elements = append(elements, string(f))
-		// Operator -> Operator
-		case 22:
-			return nil, errors.New("duplicate operator")
-		// None -> None; Operator -> None
-		default:
-		}
-		currType = t
-	}
-	if len(rs) != 0 {
-		if currType == TypeOperator {
-			return nil, errors.New("formula end with operator")
-		}
-		elements = append(elements, string(rs))
-	}
-	return elements, nil
-}
-
-func (c Calculator) Parse(elements []string, delimiters ...string) ([]string, error) {
-	if len(delimiters) == 0{
-		return elements, nil
-	}
-	results := []string{}
-	delimiter := rune(delimiters[0][0])
-	delimiters = delimiters[1:]
-	var element string
-	var i, index int	
-	var e rune
-	for _, element = range elements{
-		index = 0
-		for i, e = range element{
-			if e == delimiter{
-				results = append(results, element[index: i])
-				results = append(results, string(e))
-				index = i + 1
-			}
-		}
-		if index != len(element){
-			results = append(results, element[index:])
-		}
-	}
-	if(len(delimiters) > 0){
-		return c.Parse(results, delimiters...)
-	}
-	return results, nil
-}
-
-func (c Calculator) GetElementType(s string) int {
-	rs := []rune(s)
-	if len(rs) > 1 {
-		return TypeNumber
-	} else {
-		return c.GetType(rs[0])
-	}
-}
-
-func (c Calculator) GetType(b rune) int {
-	if (40 <= b && b <= 43) || b == 45 || b == 47 {
-		return TypeOperator
-	} else if b == 46 || (48 <= b && b <= 57) {
-		return TypeNumber
-	} else {
-		return TypeNone
-	}
 }
 
 // a + (b - c) * (d - e) / f
@@ -262,4 +77,289 @@ func (c Calculator) ComputeV2(formula string) (string, error) {
 		}
 	}
 	return c.ComputeV1(formula)
+}
+
+// a + b - c * d - e / f
+func (c Calculator) ComputeV1(formula string) (string, error) {
+	elements, err := c.ParseFormula(formula)
+	if err != nil {
+		return "", err
+	}
+	// muliply or divide
+	elements, err = c.computeAndSquash(elements, "*", "/")
+	if err != nil {
+		return "", err
+	}
+	// plus or minus
+	elements, err = c.computeAndSquash(elements, "+", "-")
+	if err != nil {
+		return "", err
+	}
+	result := "0"
+	var element string
+	for _, element = range elements {
+		result, err = c.compute(result, "+", element)
+		if err != nil {
+			return "", errors.New("invalid element of formula")
+		}
+	}
+	return result, nil
+}
+
+func (c Calculator) ParseFormula(formula string) ([]string, error) {
+	elements, err := c.Parse([]string{formula}, "*", "/")
+	if err != nil {
+		return nil, err
+	}
+	buffers := []string{}
+	var temps []string
+	var element string
+	for _, element = range elements {
+		temps, err = c.Parse([]string{strings.TrimSpace(element)}, "+")
+		if err != nil {
+			return nil, err
+		}
+		buffers = append(buffers, temps...)
+	}
+	elements = elements[:0]
+	for _, element = range buffers {
+		temps, err = c.ParseMinus(strings.TrimSpace(element))
+		if err != nil {
+			return nil, err
+		}
+		elements = append(elements, temps...)
+	}
+	return elements, nil
+}
+
+func (c Calculator) Parse(elements []string, delimiters ...string) ([]string, error) {
+	if len(delimiters) == 0 {
+		return elements, nil
+	}
+	results := []string{}
+	delimiter := rune(delimiters[0][0])
+	delimiters = delimiters[1:]
+	var element string
+	var i, index int
+	var e rune
+	for _, element = range elements {
+		index = 0
+		for i, e = range element {
+			if e == delimiter {
+				results = append(results, element[index:i])
+				results = append(results, string(e))
+				index = i + 1
+			}
+		}
+		if index != len(element) {
+			results = append(results, element[index:])
+		}
+	}
+	if len(delimiters) > 0 {
+		return c.Parse(results, delimiters...)
+	}
+	return results, nil
+}
+
+// rune: - => 45
+func (c Calculator) ParseMinus(formula string) ([]string, error) {
+	count := 0
+	var r rune
+	for _, r = range formula {
+		if r == 45 {
+			count += 1
+		}
+	}
+	if count == 0 {
+		return []string{formula}, nil
+	}
+	elements := []string{}
+	rs := []rune{}
+	var currType int = TypeNone
+	var crossType int
+	var t int
+	var err error
+	for _, r = range formula {
+		t = c.GetType(r)
+		// None, Number, Minus
+		crossType = 10*currType + t
+		switch crossType {
+		// None -> None
+		case 0:
+			// NOTE: Do nothing
+		// None -> Number
+		case 1:
+			rs = append(rs, r)
+		// None -> Minus
+		case 3:
+			rs = append(rs, r)
+		// Number -> None
+		case 10:
+			elements, rs, err = c.squashMinus(elements, rs)
+			if err != nil {
+				return nil, err
+			}
+		// Number -> Number
+		case 11:
+			rs = append(rs, r)
+		// Number -> Minus
+		case 13:
+			elements, rs, err = c.squashMinus(elements, rs)
+			if err != nil {
+				return nil, err
+			}
+			rs = append(rs, r)
+		// Minus -> None
+		case 30:
+			elements, rs, err = c.squashMinus(elements, rs)
+			if err != nil {
+				return nil, err
+			}
+		// Minus -> Number
+		case 31:
+			rs = append(rs, r)
+		// Minus -> Minus
+		case 33:
+			if len(rs) <= 1 {
+				rs = append(rs, r)
+			} else {
+				return nil, errors.New("error format of formula")
+			}
+		default:
+			return nil, errors.New("error format of formula")
+		}
+		currType = t
+	}
+	if len(rs) > 0 {
+		elements, _, err = c.squashMinus(elements, rs)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return elements, nil
+}
+
+func (c Calculator) squashMinus(elements []string, rs []rune) ([]string, []rune, error) {
+	// 0: number only
+	// 1: negative number (-x)
+	// 2: minus a negative number (- -x)
+	count := 0
+	var i int
+	var r rune
+	for i, r = range rs {
+		if r == 45 {
+			if i != 0 && i != 1 {
+				count = -1
+			}
+			count += 1
+			if count > 2 {
+				count = -1
+			}
+		}
+	}
+
+	switch count {
+	case 0:
+		fallthrough
+	case 1:
+		elements = append(elements, string(rs))
+	case 2:
+		elements = append(elements, "-")
+		elements = append(elements, string(rs[1:]))
+	default:
+		return nil, nil, errors.New("error format of formula")
+	}
+	rs = rs[:0]
+	return elements, rs, nil
+}
+
+func (c Calculator) computeAndSquash(elements []string, targets ...string) ([]string, error) {
+	index := c.FindIndex(elements, targets...)
+	var v1, v2, op, result string
+	var prev, next []string
+	var err error
+	for index != -1 {
+		if index == 0 || index == len(elements)-1 {
+			return nil, errors.New("error format of formula")
+		}
+		v1 = elements[index-1]
+		op = elements[index]
+		v2 = elements[index+1]
+		if c.GetElementType(v1) != TypeNumber || c.GetElementType(v2) != TypeNumber {
+			return nil, errors.New("error format of formula")
+		}
+		result, err = c.compute(v1, op, v2)
+		if err != nil {
+			return nil, errors.New("invalid element of formula")
+		}
+		prev = elements[:index-1]
+		next = elements[index+2:]
+		elements = append(prev, result)
+		elements = append(elements, next...)
+		index = c.FindIndex(elements, targets...)
+	}
+	return elements, nil
+}
+
+func (c Calculator) FindIndex(elements []string, targets ...string) int {
+	var t string
+	for i, e := range elements {
+		for _, t = range targets {
+			if e == t {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+func (c Calculator) compute(v1 string, op string, v2 string) (string, error) {
+	var f1, f2 float64
+	var err error
+	if f1, err = strconv.ParseFloat(v1, 64); err != nil {
+		return "", errors.New("cannot convert element to number")
+	}
+	if f2, err = strconv.ParseFloat(v2, 64); err != nil {
+		return "", errors.New("cannot convert element to number")
+	}
+	var result float64
+	switch op {
+	case "+":
+		result = f1 + f2
+	case "-":
+		result = f1 - f2
+	case "*":
+		result = f1 * f2
+	case "/":
+		result = f1 / f2
+	default:
+		return "", errors.New("invalid operator")
+	}
+	format := fmt.Sprintf("%%.%df", c.digits)
+	return fmt.Sprintf(format, result), nil
+}
+
+func (c Calculator) GetElementType(s string) int {
+	rs := []rune(s)
+	length := len(rs)
+	switch length {
+	case 0:
+		return TypeNone
+	case 1:
+		return c.GetType(rs[0])
+	default:
+		return TypeNumber
+	}
+}
+
+func (c Calculator) GetType(b rune) int {
+	if b == 45 {
+		return TypeMinus
+	} else if (40 <= b && b <= 43) || b == 47 {
+		return TypeOperator
+	} else if b == 46 || (48 <= b && b <= 57) {
+		return TypeNumber
+	} else {
+		return TypeNone
+	}
 }
--- app/main.go
+++ app/main.go
@@ -1,50 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	calculator := NewCalculator()
-	// answer, err := calculator.ComputeV1("5 + (-3)")
-	// if err != nil {
-	// 	fmt.Printf("err: %+v\n", err)
-	// 	return
-	// }
-
-	// fmt.Printf("answer: %s\n", answer)
-	results, _ := calculator.Parse([]string{"5 + 3 * 7 - 5 / 6"}, "*", "/")
-	for _, result := range results{
-		fmt.Printf("result: %+v\n", result)
-		plusResults, _ := calculator.Parse([]string{result}, "+")
-
-		for _, pr := range plusResults{
-			fmt.Printf("\tpr: %+v\n", pr)
+	server := gin.Default()
+	server.GET("/formula", func(ctx *gin.Context) {
+		value := ctx.Query("value")
+		answer, err := calculator.ComputeV2(value)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"value": value,
+				"err":   fmt.Sprintf("err: %+v", err),
+			})
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{
+				"answer": answer,
+			})
 		}
-	}
+	})
+	server.POST("/digits", func(ctx *gin.Context) {
+		var postData PostDigits
+		if err := ctx.BindJSON(&postData); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-	// server := gin.Default()
-	// server.GET("/formula", func(ctx *gin.Context) {
-	// 	value := ctx.Query("value")
-	// 	answer, err := calculator.ComputeV2(value)
-	// 	if err != nil {
-	// 		ctx.JSON(http.StatusBadRequest, gin.H{
-	// 			"value": value,
-	// 			"err":   fmt.Sprintf("err: %+v", err),
-	// 		})
-	// 	} else {
-	// 		ctx.JSON(http.StatusOK, gin.H{
-	// 			"answer": answer,
-	// 		})
-	// 	}
-	// })
-	// server.POST("/digits", func(ctx *gin.Context) {
-	// 	var postData PostDigits
-	// 	if err := ctx.BindJSON(&postData); err != nil {
-	// 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
-
-	// 	calculator.SetDigits(postData.Digits)
-	// 	ctx.JSON(http.StatusOK, gin.H{"msg": "OK"})
-	// })
-	// server.Run()
+		calculator.SetDigits(postData.Digits)
+		ctx.JSON(http.StatusOK, gin.H{"msg": "OK"})
+	})
+	server.Run()
 }
